9acc5d7c5a441edb127d4fe051aebb01def63ef2
bhavya-boda <bodabhavya00@gmail.com>
2025-03-29 19:13:53 +0530

    Portfolio Model Initialized

---
+++ backend/src/models/portfolio.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const portfolioSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+}, 
+{ 
+    timestamps: true 
+});
+
+const Portfolio = mongoose.model('Portfolio', portfolioSchema);
+
+export default Portfolio;
